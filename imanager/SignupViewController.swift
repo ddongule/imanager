982d0a162bcac1ecec19b70022bc7e98c03a8198
장준혁 <jjh7091@naver.com>
2018-05-11 17:51:47 +0900

    회원가입시 이메일 전송하는 기능을 추가함 / firebase 아이디를 통합할 필요성 있음(나중에 논의)
    firebase쓸려고 Xcode의 구글plis를 바꿈 아마 누나껄로 하면 안됨

--- imanager/SignupViewController.swift
+++ imanager/SignupViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseCore
 import FirebaseAuth
 import MessageUI
 import FirebaseDatabase
@@ -15,7 +16,10 @@ class SignupViewController: UIViewController, UITextFieldDelegate, MFMailCompose
     
     @IBOutlet weak var sendMailButton: UIButton!
     @IBAction func sendMail(_ sender: Any) {
+        
+        //Why email 인증이랑 회원가입이랑 따로 만들었지??
     }
+    
     //    var ref: DatabaseReference!
     var ref = Database.database().reference()
     
@@ -31,12 +35,9 @@ class SignupViewController: UIViewController, UITextFieldDelegate, MFMailCompose
     
 
     
-    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!   //버튼을 why? Outlet으로 만들었지?
     @IBAction func signUpTapped(_ sender: Any) {
-        if (emailTextField.text?.isEmpty)! ||
-            (passwordTextField.text?.isEmpty)! ||
-            (nameTextField.text?.isEmpty)! ||
-            (passwordConfirmTextField.text?.isEmpty)!
+        if (emailTextField.text?.isEmpty)! || (passwordTextField.text?.isEmpty)! || (nameTextField.text?.isEmpty)! || (passwordConfirmTextField.text?.isEmpty)!
         { displayAlert(title: "정보를 기입해 주세요.", message: "이메일과 비밀번호를 모두 입력하셔야 합니다.")
         } else if ((passwordTextField.text?.elementsEqual(passwordConfirmTextField.text!))! != true)
         {
@@ -46,27 +47,54 @@ class SignupViewController: UIViewController, UITextFieldDelegate, MFMailCompose
                 if let password = passwordTextField.text {
                     
                     // SIGN UP
-                    Auth.auth().createUser(withEmail: email, password: password, completion: {(user, error) in
+                    Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
                         if error != nil {
-                            self.displayAlert(title: "Error", message: error!.localizedDescription)
-                            
-                        } else {
-                            let myAlert = UIAlertController(title: "회원가입 되었습니다.", message: "", preferredStyle: UIAlertControllerStyle.alert)
-                            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default ) { action in
-                                self.dismiss(animated: true, completion: nil)}
-                            
-                            // ok 말고 cancel도 하려면 이렇게 하면 된다. 근데 cancel 해도 회원가입 됨 ㅋ 이걸 해결할 수 잇으면 좋으련만
-                            //   let cancel = UIAlertAction(title: "cancel", style: UIAlertActionStyle.cancel, handler: nil)
-                            //   myAlert.addAction(cancel)
-                            
-                            myAlert.addAction(okAction)
-                            self.present(myAlert, animated: true, completion: nil)
-                            
-                            // print("회원가입 되었습니다.")
-                            // self.displayAlert(title: "회원가입 되었습니다.", message: "")
-                            // print("회원가입 되었습니다.")
+                            let signuperrorAlert = UIAlertController(title: "Signup error", message: "\(error?.localizedDescription) Please try again later", preferredStyle: .alert)
+                            self.present(signuperrorAlert, animated: true, completion: nil)
+                            return
                         }
+                        self.sendEmail()
+                        self.dismiss(animated: true, completion: nil)
                     })
+                    
+//                    Auth.auth().createUser(withEmail: email, password: password, completion: {(user, error) in
+//                        if user != nil
+//                        {
+//                           print("SUCCESS")
+//                        }
+//                        else
+//                        {
+//                            if let myError = error?.localizedDescription
+//                            {
+//                                print(myError)
+//                            }
+//                            else
+//                            {
+//                                print("ERROR")
+//                            }
+//                        }
+//                    })
+//                    Auth.auth().createUser(withEmail: email, password: password, completion: {(user, error) in
+//                        if error != nil {
+//                            self.displayAlert(title: "Error", message: error!.localizedDescription)
+//
+//                        } else {
+//                            let myAlert = UIAlertController(title: "회원가입 되었습니다.", message: "", preferredStyle: UIAlertControllerStyle.alert)
+//                            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default ) { action in
+//                                self.dismiss(animated: true, completion: nil)}
+//
+//                            // ok 말고 cancel도 하려면 이렇게 하면 된다. 근데 cancel 해도 회원가입 됨 ㅋ 이걸 해결할 수 잇으면 좋으련만
+//                            //   let cancel = UIAlertAction(title: "cancel", style: UIAlertActionStyle.cancel, handler: nil)
+//                            //   myAlert.addAction(cancel)
+//
+//                            myAlert.addAction(okAction)
+//                            self.present(myAlert, animated: true, completion: nil)
+//
+//                            // print("회원가입 되었습니다.")
+//                            // self.displayAlert(title: "회원가입 되었습니다.", message: "")
+//                            // print("회원가입 되었습니다.")
+//                        }
+//                    })
                 }}
         }
     }
@@ -77,9 +105,32 @@ class SignupViewController: UIViewController, UITextFieldDelegate, MFMailCompose
         self.present(alertController, animated: true, completion: nil)
     }
     
-    // 이메일 보내기 - 아직 못함
-    
-    
+    // 이메일 보내기 - 아직 못함 -> 5/11 장준혁 시도: 성공
+    func sendEmail() {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil {
+                print("Error: \(String(describing:error!.localizedDescription))")
+                return
+            }
+            Auth.auth().currentUser?.sendEmailVerification(completion: { (error) in
+                if error != nil {
+                    let emailNOTSentAlert = UIAlertController(title: "Email Verification", message: "Verification email failed to send:\(error?.localizedDescription)", preferredStyle: .alert); emailNOTSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil)); self.present(emailNOTSentAlert, animated: true, completion: nil)
+                }else{
+                    let emailSentAlert = UIAlertController(title: "Email Verification", message: "Verification email has been sent. Please tap on the link in the email to verify your account before you can use the features in the app", preferredStyle: .alert)
+                    emailSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(emailSentAlert, animated: true, completion: {
+                        self.dismiss(animated: true, completion: nil)
+                    })
+                }
+                do {
+                    try Auth.auth().signOut()
+                } catch {
+                    //ERROR HANDLING
+                }
+            })
+        }
+
+    }
     
     
     override func viewDidLoad() {
